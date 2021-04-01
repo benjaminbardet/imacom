e6a83b8f560e2b87e0164049c7c465dd9e0f555c
Malo <malolemestre29@gmail.com>
2021-04-01 10:20:24 +0200

    PostMini V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PosteMiniComponent } from './poste-mini/poste-mini.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PosteMiniComponent
   ],
   imports: [
     BrowserModule
