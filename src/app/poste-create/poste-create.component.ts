8985c1bc9d28035dfda017fab19d39a905c2d168
benjamin Bardet <benjamin.bardet@etu.univ-orleans.fr>
2021-04-07 10:43:23 +0200

    ajout de fichier

---
+++ src/app/poste-create/poste-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poste-create',
+  templateUrl: './poste-create.component.html',
+  styleUrls: ['./poste-create.component.css']
+})
+export class PosteCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
